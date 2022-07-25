5f0f0c589d9627ec8b3f0141e3bd60b0fc371776
Niccatania <Niccatania6@gmail.com>
2022-07-25 11:07:42 -0700

    Updating server.js config to handle Apollo server and requring typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use((req, res) => {
+    res.status(200);
+    res.send('Hello!');
+    res.end();
+  });
+
+}
+
+// These middleware functions let Incoming requests be recognized as a string or json object
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +37,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*',(req,res)=>
+res.sendFile(path.join(__dirname, 'client/build/index.html')));
 
+// Once our server is open lets log the port it is running on
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
 });
+
+// Function call to start the Apollo server
+startApolloServer(typeDefs, resolvers);
