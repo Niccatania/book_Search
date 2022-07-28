fc024a65b465a36def29e4b8bcc66064cb3a78c1
Niccatania <Niccatania6@gmail.com>
2022-07-28 10:53:55 -0700

    route update

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const path = require('path');
 
 router.use('/users', userRoutes);
+
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
 
 module.exports = router;
