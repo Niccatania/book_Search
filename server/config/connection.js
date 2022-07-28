55352d7af2f689db6306fe6bdbf2352109428669
Niccatania <Niccatania6@gmail.com>
2022-07-27 23:14:42 -0700

    change mongo link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book_SearchDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book_SearchDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
